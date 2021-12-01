c3ed85c075ebf4ce4c4c53da3d546aa92a653d56
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-12-01 18:53:07 +0200

    Update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,20 @@
+process.env.RUNNING_PRETTIER = 'true';
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
   printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
   tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
 };
