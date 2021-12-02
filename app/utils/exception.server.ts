6b4b3058c8470bcc222dccd6390524a63e493cb4
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-12-03 00:26:40 +0200

    Make exception type safe

--- app/utils/exception.server.ts
+++ app/utils/exception.server.ts
@@ -1,14 +1,30 @@
+import { isNumber, isObject, isString } from '@tool-belt/type-predicates'
+
 import { Exception } from '~/api'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getException = (error: any): Exception => {
+function hasProp<K extends PropertyKey>(
+  data: object,
+  prop: K,
+): data is Record<K, unknown> {
+  return prop in data
+}
+
+export const getException = (error: unknown): Exception => {
+  if (!isObject(error) || !hasProp(error, 'error')) throw error
+
+  const { error: unkownException } = error
+
+  if (!isObject(unkownException)) throw error
+
   if (
-    typeof error === 'object' &&
-    'error' in error &&
-    'statusCode' in error.error &&
-    'message' in error.error
-  ) {
-    return error.error
-  }
-  throw error
+    !hasProp(unkownException, 'message') ||
+    !hasProp(unkownException, 'statusCode')
+  )
+    throw error
+
+  const { message, statusCode } = unkownException
+
+  if (!isNumber(statusCode) || !isString(message)) throw error
+
+  return { message, statusCode }
 }
